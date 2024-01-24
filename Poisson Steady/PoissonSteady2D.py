4cb913d76f40857a91c66cd7ca509ea82c6234f5
juliancastrillon77 <julian-castrillon@hotmail.com>
2024-01-23 21:15:27 -0800

    Change in Steady Poisson 2D

--- Poisson Steady/PoissonSteady2D.py
+++ Poisson Steady/PoissonSteady2D.py
@@ -8,52 +8,61 @@ def clear_console():
     os.system('cls' if os.name == 'nt' else 'clear')
 clear_console()
 
-##
+MeshFile  = 'Mesh.xml' # Input mesh
+FacetFile = 'Mesh_facet_region.xml' # Input mesh boundaries
+OutFileu  = 'Results/u.pvd'  # Output Storage
 
-mesh = fe.Mesh('Domain2D.xml')
-V = fe.FunctionSpace(mesh, 'Lagrange', 1)
+Mesh = fe.Mesh(MeshFile)     # Create the mesh and import mesh into the solver
+FS = fe.FunctionSpace(Mesh, 'Lagrange', 1)
 
-## Boundaries
-DomainBoundaries = fe.MeshFunction('size_t', mesh, 'Domain2D_facet_region.xml') # size_t ?
+u = fe.TrialFunction(FS)
+w = fe.TestFunction(FS)
 
-Entry = 35
-Exit1 = 37
-Exit2 = 36
-Walls = 38
+DomainBoundaries = fe.MeshFunction('size_t', Mesh, FacetFile)
+ds = fe.ds(subdomain_data = DomainBoundaries)
+
+Entry         = 8
+BottomWall    = 9
+Exit          = 10
+TopWall       = 11
+Circle        = 12
+TriangleLeft  = 13
+TriangleRight = 14
 
 Expres = fe.Expression('x[1]*(1-x[1])', degree = 2)
-Const = fe.Constant(0)
+Const  = fe.Constant(0)
 
-EntryBC = fe.DirichletBC(V, Const, DomainBoundaries,Entry)
-Exit1BC = fe.DirichletBC(V, Const, DomainBoundaries,Exit1)
-Exit2BC = fe.DirichletBC(V, Const, DomainBoundaries,Exit2)
-WallsBC = fe.DirichletBC(V, Expres, DomainBoundaries,Walls)
-BCset = [EntryBC,Exit1BC,Exit2BC,WallsBC]
+EntryBC         = fe.DirichletBC(FS, Const,  DomainBoundaries, Entry)
+BottomWallBC    = fe.DirichletBC(FS, Const,  DomainBoundaries, BottomWall)
+ExitBC          = fe.DirichletBC(FS, Const,  DomainBoundaries, Exit)
+TopWallBC       = fe.DirichletBC(FS, Const,  DomainBoundaries, TopWall)
+CircleBC        = fe.DirichletBC(FS, Expres, DomainBoundaries, Circle)
+TriangleLeftBC  = fe.DirichletBC(FS, Expres, DomainBoundaries, TriangleLeft)
+TriangleRightBC = fe.DirichletBC(FS, Expres, DomainBoundaries, TriangleRight)
 
-## Define Weak Formulation
-u = fe.TrialFunction(V)
-w = fe.TestFunction(V)
+BCs = [EntryBC, BottomWallBC, ExitBC, TopWallBC, CircleBC, TriangleLeftBC, TriangleRightBC]
 
 a = fe.dot(fe.grad(w), fe.grad(u))*fe.dx
 f = fe.Expression('x[0]*x[1]', degree = 2)
 b = w*f*fe.dx
 
-Solution = fe.Function(V)
-#fe.solve(a==-b, Solution, BCset)
+TF = fe.Function(FS)
 
-Problem = fe.LinearVariationalProblem(a, -b, Solution, BCset)
+Problem = fe.LinearVariationalProblem(a, -b, TF, BCs)
 Solver  = fe.LinearVariationalSolver(Problem)
+
 Solver.parameters['linear_solver'] = 'cg'
-Solver.parameters['preconditioner']  = 'sor'
+Solver.parameters['preconditioner']  = 'ilu'
 Solver.parameters['krylov_solver']['monitor_convergence'] = True
 Solver.parameters['krylov_solver']['relative_tolerance'] = fe.Constant(1e-6)
 Solver.parameters['krylov_solver']['absolute_tolerance'] = fe.Constant(1e-8)
 Solver.solve()
 
-vtkFile = fe.File('Results/PoissonSteady2D.pvd')
-Solution.rename('u','')
+uFile = fe.File(OutFileu)
+TF.rename('u','u')
+uFile << TF
 
-vtkFile << Solution
+
 
 
 
