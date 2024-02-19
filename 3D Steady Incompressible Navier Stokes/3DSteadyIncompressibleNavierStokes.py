12c71902e1359e9b9f205d8b4bfa77130df314a8
juliancastrillon77 <julian-castrillon@hotmail.com>
2024-02-18 18:23:38 -0800

    3D Starting

---
+++ 3D Steady Incompressible Navier Stokes/3DSteadyIncompressibleNavierStokes.py
@@ -0,0 +1,101 @@
+# Julian Castrillon
+# CFD - 3D Steady Incompressible Navier Stokes Equations
+
+import os
+import fenics as fe
+
+def clear_console():
+    os.system('cls' if os.name == 'nt' else 'clear')
+clear_console()
+
+fe.set_log_level(fe.LogLevel.PROGRESS)
+fe.parameters['form_compiler']['representation']     = 'uflacs'
+fe.parameters['form_compiler']['optimize']           = True
+fe.parameters['form_compiler']['cpp_optimize']       = True
+fe.parameters["form_compiler"]["cpp_optimize_flags"] = '-O2 -funroll-loops'
+
+Mesh = fe.Mesh('3DMesh.xml')
+
+vi  = 0.2                        # Inlet velocity [m/s]
+mu  = fe.Constant(1)             # Dynamic viscosity  [kg/ms]
+rho = fe.Constant(10)            # Density            [kg/m3]
+b   = fe.Constant((0, -9.81, 0)) # Body accelerations [m/s2]
+
+Vel  = fe.VectorElement('Lagrange', Mesh.ufl_cell(), 2)
+Pres = fe.FiniteElement('Lagrange', Mesh.ufl_cell(), 1)
+M    = fe.MixedElement([Vel, Pres])
+FS   = fe.FunctionSpace(Mesh, M)
+
+TF     = fe.TrialFunction(FS)
+(w, q) = fe.TestFunctions(FS)
+
+TFsol  = fe.Function(FS)
+(v, p) = fe.split(TFsol)
+
+WeakForm =   rho*fe.inner(w,fe.grad(v)*v)*fe.dx       \
+           - fe.div(w)*p*fe.dx                        \
+           - rho*fe.dot(w,b)*fe.dx                    \
+           + mu*fe.inner(fe.grad(w),fe.grad(v))*fe.dx \
+           + q*fe.div(v)*fe.dx
+
+J = fe.derivative(WeakForm, TFsol, TF)  
+
+DomainBoundaries = fe.MeshFunction('size_t', Mesh, '3DMesh_facet_region.xml')
+
+LeftWall            = 46
+RightWall           = 47
+Entry               = 48
+Exit                = 49
+BottomWall          = 50
+TopWall             = 51
+SphereForwardRight  = 52
+SphereForwardLeft   = 53
+SphereBackwardLeft  = 54
+SphereBackwardRight = 55
+ConeForwardRight    = 56
+ConeForwardLeft     = 57
+ConeBackwardLeft    = 58
+ConeBackwardRight   = 59
+
+NoSlip    = fe.Constant((0, 0, 0))
+POut      = fe.Constant(0)
+InletFlow = fe.Constant((vi, 0, 0))
+
+LeftWallBC            = fe.DirichletBC(FS.sub(0), NoSlip,    DomainBoundaries, LeftWall)
+RightWallBC           = fe.DirichletBC(FS.sub(0), NoSlip,    DomainBoundaries, RightWall)
+EntryBC               = fe.DirichletBC(FS.sub(0), InletFlow, DomainBoundaries, Entry)
+ExitBC                = fe.DirichletBC(FS.sub(1), POut,      DomainBoundaries, Exit)
+BottomWallBC          = fe.DirichletBC(FS.sub(0), NoSlip,    DomainBoundaries, BottomWall)
+TopWallBC             = fe.DirichletBC(FS.sub(0), NoSlip,    DomainBoundaries, TopWall)
+SphereForwardRightBC  = fe.DirichletBC(FS.sub(0), NoSlip,    DomainBoundaries, SphereForwardRight)
+SphereForwardLeftBC   = fe.DirichletBC(FS.sub(0), NoSlip,    DomainBoundaries, SphereForwardLeft)
+SphereBackwardLeftBC  = fe.DirichletBC(FS.sub(0), NoSlip,    DomainBoundaries, SphereBackwardLeft)
+SphereBackwardRightBC = fe.DirichletBC(FS.sub(0), NoSlip,    DomainBoundaries, SphereBackwardRight)
+ConeForwardRightBC    = fe.DirichletBC(FS.sub(0), NoSlip,    DomainBoundaries, ConeForwardRight)
+ConeForwardLeftBC     = fe.DirichletBC(FS.sub(0), NoSlip,    DomainBoundaries, ConeForwardLeft)
+ConeBackwardLeftBC    = fe.DirichletBC(FS.sub(0), NoSlip,    DomainBoundaries, ConeBackwardLeft)
+ConeBackwardRightBC   = fe.DirichletBC(FS.sub(0), NoSlip,    DomainBoundaries, ConeBackwardRight)
+
+BCs = [LeftWallBC, RightWallBC, EntryBC, ExitBC, BottomWallBC, TopWallBC, SphereForwardRightBC, \
+       SphereForwardLeftBC, SphereBackwardLeftBC, SphereBackwardRightBC, ConeForwardRightBC,    \
+       ConeForwardLeftBC, ConeBackwardLeftBC, ConeBackwardRightBC]
+
+Problem = fe.NonlinearVariationalProblem(WeakForm, TFsol, BCs, J)
+Solver  = fe.NonlinearVariationalSolver(Problem)
+
+Parameters = Solver.parameters
+Parameters['newton_solver']['absolute_tolerance']   = 1e-8
+Parameters['newton_solver']['relative_tolerance']   = 1e-7
+Parameters['newton_solver']['maximum_iterations']   = 7
+Parameters['newton_solver']['relaxation_parameter'] = 1.0
+
+FileVel  = fe.File('Results/Velocity.pvd')
+FilePres = fe.File('Results/Pressure.pvd')
+
+Solver.solve()
+
+(Velocity, Pressure) = TFsol.split(deepcopy = True)
+Velocity.rename('Velocity','Velocity')
+Pressure.rename('Pressure','Pressure')
+FileVel  << Velocity
+FilePres << Pressure
---
+++ ZMesh/3D Mesh/3DMesh.geo
@@ -0,0 +1,88 @@
+// Julian Castrillon
+// 3D Mesh
+
+h = 0.3;
+
+//+
+Point(1)  = {0, 0, 0, h};
+Point(2)  = {5, 0, 0, h};
+Point(3)  = {5, 1, 0, h};
+Point(4)  = {0, 1, 0, h};
+Point(5)  = {0, 0, 1, h};
+Point(6)  = {5, 0, 1, h};
+Point(7)  = {5, 1, 1, h};
+Point(8)  = {0, 1, 1, h};
+Point(9)   = {1.5, 0.2, 0.5, h};
+Point(10)  = {1.3, 0.6, 0.5, h};
+Point(11)  = {1.5, 0.6, 0.5, h};
+Point(12)  = {1.5, 0.8, 0.5, h};
+
+//+
+Line(1) = {1, 2};
+Line(2) = {2, 3};
+Line(3) = {3, 4};
+Line(4) = {4, 1};
+Line(5) = {5, 6};
+Line(6) = {6, 7};
+Line(7) = {7, 8};
+Line(8) = {8, 5};
+Line(9) = {1, 5};
+Line(10) = {8, 4};
+Line(11) = {2, 6};
+Line(12) = {7, 3};
+Line(13) = {9, 10};
+Circle(14) = {10, 11, 12};
+
+//+
+Extrude {{0, 1, 0}, {1.5, 0.6, 0.5}, Pi/2} {
+  Curve{13}; Curve{14}; 
+}
+Extrude {{0, 1, 0}, {1.5, 0.6, 0.5}, Pi/2} {
+  Curve{15}; Curve{18}; 
+}
+Extrude {{0, 1, 0}, {1.5, 0.6, 0.5}, Pi/2} {
+  Curve{21}; Curve{24}; 
+}
+Extrude {{0, 1, 0}, {1.5, 0.6, 0.5}, Pi/2} {
+  Curve{27}; Curve{30}; 
+}
+
+//+
+Curve Loop(1) = {1, 2, 3, 4};
+Plane Surface(40) = {1}; // Left Wall
+Curve Loop(2) = {5, 6, 7, 8};
+Plane Surface(41) = {2}; // Right Wall
+Curve Loop(3) = {9, -8, 10, 4};
+Plane Surface(42) = {3}; // Entry Wall
+Curve Loop(4) = {11, 6, 12, -2};
+Plane Surface(43) = {4}; // exit Wall
+Curve Loop(5) = {1, 11, -5, -9};
+Plane Surface(44) = {5}; // Bottom Wall
+Curve Loop(6) = {3, -10, -7, 12};
+Plane Surface(45) = {6}; // Top Wall
+
+//+
+Surface Loop(1) = {40, 44, 43, 41, 45, 42};
+Surface Loop(2) = {20, 26, 32, 38, 35, 29, 23, 17};
+Volume(1) = {1, 2};
+
+//+
+Physical Surface("LeftWall", 46) = {40};
+Physical Surface("RightWall", 47) = {41};
+Physical Surface("Entry", 48) = {42};
+Physical Surface("Exit", 49) = {43};
+Physical Surface("BottomWall", 50) = {44};
+Physical Surface("TopWall", 51) = {45};
+
+//+
+Physical Surface("SphereForwardRight", 52) = {26};
+Physical Surface("SphereForwardLeft", 53) = {32};
+Physical Surface("SphereBackwardLeft", 54) = {38, 41};
+Physical Surface("SphereBackwardRight", 55) = {20};
+Physical Surface("ConeForwardRight", 56) = {41, 23};
+Physical Surface("ConeForwardLeft", 57) = {29};
+Physical Surface("ConeBackwardLeft", 58) = {35};
+Physical Surface("ConeBackwardRight", 59) = {17};
+Physical Volume("Vol", 60) = {1};
+
+
